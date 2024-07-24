ae50e281ffb405a275c6b7a447531bd999ff941d
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-07-24 01:15:30 -0300

    versao 1.05

---
+++ static/js/myscript.js
@@ -0,0 +1,14 @@
+//codigo da navbar ao rolar a pagina
+
+const nav = document.querySelector('nav')
+
+        document.addEventListener("scroll",e=>{
+            if(scrollY>100){
+                if(scrollY>window.innerHeight){
+                nav.classList.add('invisible')
+            }else{
+
+                nav.classList.remove('invisible')
+            }
+            }
+        })
