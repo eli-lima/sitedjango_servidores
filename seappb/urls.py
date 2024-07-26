ca4d215ce05d413a5b09d99d349b5979012efc7d
elyai <elyaislan@gmail.com>
2024-07-25 21:58:04 -0300

    1.08

--- seappb/urls.py
+++ seappb/urls.py
@@ -2,11 +2,14 @@
 
 from django.urls import path, include
 from .views import Homepage, PesquisarSite
+from django.contrib.auth import views as auth_view
 
 app_name = 'seappb'
 
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('pesquisa/', PesquisarSite.as_view(), name='pesquisarsite'),
+    path('login/', auth_view.LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name="logout.html"), name='logout'),
 
 ]
