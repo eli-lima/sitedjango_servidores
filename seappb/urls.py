59ed8f8d2eb0fca3b97150b73771204d81629fd8
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-07-27 07:59:29 -0300

    versao 1.12

--- seappb/urls.py
+++ seappb/urls.py
@@ -1,7 +1,7 @@
 #urls - view - template
 
 from django.urls import path, include
-from .views import Homepage, PesquisarSite, Paginaperfil, Criarconta
+from .views import Homepage, PesquisarSite, Paginaperfil, Criarconta, CustomLoginView
 from django.contrib.auth import views as auth_view
 
 app_name = 'seappb'
@@ -9,7 +9,7 @@ app_name = 'seappb'
 urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('pesquisa/', PesquisarSite.as_view(), name='pesquisarsite'),
-    path('login/', auth_view.LoginView.as_view(template_name="login.html"), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name="logout.html"), name='logout'),
     path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
     path('criarconta/', Criarconta.as_view(), name='criarconta')
