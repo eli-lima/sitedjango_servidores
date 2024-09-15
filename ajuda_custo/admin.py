4f2db38976c20b16be79e22829be3331cbe992ac
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-09-15 15:02:31 -0300

    imagens servidor

---
+++ ajuda_custo/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import DataMajorada, Ajuda_Custo, LimiteAjudaCusto
+
+# Register your models here.
+
+admin.site.register(DataMajorada)
+
+admin.site.register(Ajuda_Custo)
+
+admin.site.register(LimiteAjudaCusto)
+
+
---
+++ ajuda_custo/forms.py
@@ -0,0 +1,100 @@
+# forms.py
+from django import forms
+from .models import Ajuda_Custo, LimiteAjudaCusto
+import datetime
+from seappb.models import Unidade
+from django.core.exceptions import ValidationError
+
+
+MESES = [
+    ('', '--- Selecione um Mês ---'),
+    ('01', 'Janeiro'),
+    ('02', 'Fevereiro'),
+    ('03', 'Março'),
+    ('04', 'Abril'),
+    ('05', 'Maio'),
+    ('06', 'Junho'),
+    ('07', 'Julho'),
+    ('08', 'Agosto'),
+    ('09', 'Setembro'),
+    ('10', 'Outubro'),
+    ('11', 'Novembro'),
+    ('12', 'Dezembro'),
+]
+
+ANOS = [('', '--- Selecione um Ano ---')] + [(str(year), str(year)) for year in range(2023, datetime.datetime.now().year + 2)]
+
+DIAS = [(str(day), str(day)) for day in range(1, 32)]
+
+
+def get_mes_field():
+    return forms.ChoiceField(choices=MESES, required=True)
+
+def get_ano_field():
+    return forms.ChoiceField(choices=ANOS, required=True)
+
+def get_unidade_field():
+    return forms.ChoiceField(
+        choices=[('', '--- Selecione uma unidade ---')] + [(u.nome, u.nome) for u in Unidade.objects.all()]
+    )
+
+class AjudaCustoForm(forms.ModelForm):
+    unidade = get_unidade_field()
+    dia = forms.ChoiceField(choices=DIAS, required=True)
+    mes = get_mes_field()
+    ano = get_ano_field()
+    class Meta:
+        model = Ajuda_Custo
+        fields = ['unidade', 'carga_horaria']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        dia = cleaned_data.get('dia')
+        mes = cleaned_data.get('mes')
+        ano = cleaned_data.get('ano')
+
+        # Combina o dia, mês e ano para formar uma data
+        try:
+            data = datetime.date(int(ano), int(mes), int(dia))
+            cleaned_data['data'] = data
+        except ValueError:
+            raise forms.ValidationError('Data inválida. Verifique o dia, mês e ano selecionados.')
+
+        return cleaned_data
+
+
+class AdminDatasForm(forms.Form):
+    matricula = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Somente Números...', 'id': 'matricula'}),
+        required=True
+    )
+    mes = get_mes_field()
+    ano = get_ano_field()
+    unidade = get_unidade_field()
+    dias_12h = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Dias separados por vírgulas...', 'id': 'dias_12h'}),
+        required=False
+    )
+    dias_24h = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Dias separados por vírgulas...', 'id': 'dias_24h'}),
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Adiciona IDs aos campos para uso no JavaScript
+        self.fields['mes'].widget.attrs.update({'id': 'mes'})
+        self.fields['ano'].widget.attrs.update({'id': 'ano'})
+        self.fields['unidade'].widget.attrs.update({'id': 'unidade'})
+
+
+class LimiteAjudaCustoForm(forms.ModelForm):
+    class Meta:
+        model = LimiteAjudaCusto
+        fields = ['servidor', 'limite_horas']
+
+        # Defina os widgets para adicionar o placeholder
+        widgets = {
+            'limite_horas': forms.NumberInput(attrs={'placeholder': 'Digite o limite em horas'}),
+            'servidor': forms.Select(attrs={'placeholder': 'Selecione o servidor'}),
+        }
