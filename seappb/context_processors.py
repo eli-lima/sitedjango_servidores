4c1fb90aefcb81dfec9ca2669d037dd4f9c7af3c
eli-lima <elyaislan@gmail.com>
2024-09-26 17:35:25 -0300

    ajuda custo template

--- seappb/context_processors.py
+++ seappb/context_processors.py
@@ -3,17 +3,29 @@
 def user_group(request):
     if request.user.is_authenticated:
         user = request.user
+        # Obter todos os grupos do usuário
+        user_groups = user.groups.values_list('name', flat=True)
 
-        # Verificando o grupo do usuário
-        if user.groups.filter(name='Administrador').exists():
-            return {'user_role': 'Administrador'}
-        elif user.groups.filter(name='GerGesipe').exists():
-            return {'user_role': 'Gerência Gesipe'}
-        elif user.groups.filter(name='GerRh').exists():
-            return {'user_role': 'Gerência Recursos Humanos'}
-        elif user.groups.filter(name='CoordGesipe').exists():
-            return {'user_role': 'Coordenador'}
-        elif user.groups.filter(name='Padrao').exists():
-            return {'user_role': 'Servidor'}
-        # Adicione mais grupos conforme necessário
-    return {'user_role': 'Usuário Padrão'}  # Caso o usuário não esteja logado ou não tenha um grupo específico
+        # Definir o papel principal (role) com base nos grupos
+        if 'Administrador' in user_groups:
+            user_role = 'Administrador'
+        elif 'GerGesipe' in user_groups:
+            user_role = 'Gerência Gesipe'
+        elif 'GerRh' in user_groups:
+            user_role = 'Gerência Recursos Humanos'
+        elif 'CoordGesipe' in user_groups:
+            user_role = 'Coordenador'
+        elif 'Padrao' in user_groups:
+            user_role = 'Servidor'
+        else:
+            user_role = 'Usuário Padrão'
+
+        return {
+            'user_groups': list(user_groups),  # Retorna todos os grupos do usuário
+            'user_role': user_role  # Retorna o papel principal
+        }
+
+    return {
+        'user_groups': [],  # Caso o usuário não esteja logado ou não tenha grupos
+        'user_role': 'Usuário Padrão'  # Papel padrão para usuários não autenticados
+    }
