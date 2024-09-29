200ab223910f426e375bc1ffa5f983bc0c1dd99c
eli-lima <elyaislan@gmail.com>
2024-09-29 13:30:39 -0300

    home servidor

--- gesipe/novos_contexts.py
+++ gesipe/novos_contexts.py
@@ -6,3 +6,22 @@ def lista_edicoes_recentes(request):
 
     return {"lista_edicoes_recentes": lista_ultimas_edicoes}
 
+
+
+
+# from ajuda_custo.models import DataMajorada  # Importe o modelo correto
+# from datetime import datetime
+#
+# # Lista de datas no formato dd/mm/yyyy
+# datas = [
+#     "01/01/2029", "05/01/2029", "06/01/2029", "07/01/2029",
+#     "12/01/2029", "13/01/2029", "14/01/2029", "19/01/2029",
+#
+#
+# ]
+#
+#
+# for data_str in datas:
+#     data_formatada = datetime.strptime(data_str, "%d/%m/%Y").date()
+#     DataMajorada.objects.get_or_create(data=data_formatada)
+#     print(f"Data {data_str} inserida com sucesso.")
