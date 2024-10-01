f32f3052eacdcea49759c800a70d5f186e423678
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-10-01 15:53:47 -0300

    erros

--- ajuda_custo/views.py
+++ ajuda_custo/views.py
@@ -421,89 +421,90 @@ class AjudaCustoAdicionar(LoginRequiredMixin, FormView):
         form = self.get_form()
 
         if form.is_valid():
-            mes = request.POST.get('mes')
-            ano = request.POST.get('ano')
-            dias = request.POST.getlist('dia')
-            unidades = request.POST.getlist('unidade')
-            cargas_horarias = request.POST.getlist('carga_horaria')
-
-            # Obter o servidor com base no usuário logado
             try:
+                mes = request.POST.get('mes')
+                ano = request.POST.get('ano')
+                dias = request.POST.getlist('dia')
+                unidades = request.POST.getlist('unidade')
+                cargas_horarias = request.POST.getlist('carga_horaria')
+
+                # Obter o servidor com base no usuário logado
                 servidor = Servidor.objects.get(matricula=self.request.user.matricula)
+
+                limite = LimiteAjudaCusto.objects.get(servidor=servidor)
+                limite_horas = limite.limite_horas
+
+                mes_int = int(mes)
+                ano_int = int(ano)
+
+                # Formatar o mês/ano para usar na verificação e no caminho do arquivo
+                mes_ano_str = f"{ano_int}-{mes_int:02d}"
+
+                folha_assinada = form.cleaned_data.get('folha_assinada')  # Obter o arquivo de folha assinada
+                ajuda_custo_existente = None  # Para rastrear se já há registros no mês
+
+                for dia, unidade, carga_horaria in zip(dias, unidades, cargas_horarias):
+                    try:
+                        data_completa = datetime.strptime(f"{dia}/{mes}/{ano}", "%d/%m/%Y").date()
+
+                        # Verificar se o servidor já marcou essa data, independentemente da carga horária
+                        if Ajuda_Custo.objects.filter(matricula=servidor.matricula, data=data_completa).exists():
+                            messages.error(self.request, f'O servidor já possui uma entrada para {dia}/{mes}/{ano}.')
+                            return self.form_invalid(form)
+
+                        # Verificar se já existe um registro para este mês (por matrícula)
+                        if not ajuda_custo_existente:
+                            ajuda_custo_existente = Ajuda_Custo.objects.filter(
+                                matricula=servidor.matricula,
+                                data__year=ano_int,
+                                data__month=mes_int
+                            ).first()
+
+                        # Cria o objeto Ajuda_Custo e salva no banco de dados, sem o arquivo de folha ainda
+                        ajuda_custo = Ajuda_Custo(
+                            matricula=self.request.user.matricula,
+                            nome=self.request.user.nome_completo,
+                            data=data_completa,
+                            unidade=unidade,
+                            carga_horaria=carga_horaria,
+                            majorado=DataMajorada.objects.filter(data=data_completa).exists(),
+                        )
+                        ajuda_custo.save()
+
+                    except ValueError:
+                        messages.error(self.request, f'Erro: Data inválida - {dia}/{mes}/{ano}')
+                        return self.form_invalid(form)
+
+                # Fora do loop, salvamos o arquivo apenas uma vez para o mês
+                if folha_assinada:
+                    if ajuda_custo_existente:
+                        # Se já havia um registro anterior, substituímos o arquivo
+                        ajuda_custo_existente.folha_assinada = folha_assinada
+                        ajuda_custo_existente.save()
+                    else:
+                        # Atribuímos o arquivo ao último registro salvo (ou poderia ser o primeiro)
+                        ajuda_custo.folha_assinada = folha_assinada
+                        ajuda_custo.save()
+
+                messages.success(self.request, 'Datas adicionadas com sucesso!')
+                return redirect(self.success_url)
+
             except Servidor.DoesNotExist:
                 messages.error(self.request, 'Erro: Servidor não encontrado.')
                 return redirect(self.success_url)
 
-            try:
-                limite = LimiteAjudaCusto.objects.get(servidor=servidor)
-                limite_horas = limite.limite_horas
             except LimiteAjudaCusto.DoesNotExist:
                 messages.error(self.request, 'Limite de horas não definido. Contate o administrador.')
                 return redirect(self.success_url)
 
-            mes_int = int(mes)
-            ano_int = int(ano)
+            except Exception as e:
+                logging.error(f"Erro ao adicionar Ajuda_Custo: {str(e)}")
+                messages.error(self.request, f'Erro: {str(e)}')
+                return redirect(self.success_url)
 
-            # Formatar o mês/ano para usar na verificação e no caminho do arquivo
-            mes_ano_str = f"{ano_int}-{mes_int:02d}"
-
-            folha_assinada = form.cleaned_data.get('folha_assinada')  # Obter o arquivo de folha assinada
-            ajuda_custo_existente = None  # Para rastrear se já há registros no mês
-
-            for dia, unidade, carga_horaria in zip(dias, unidades, cargas_horarias):
-                try:
-                    data_completa = datetime.strptime(f"{dia}/{mes}/{ano}", "%d/%m/%Y").date()
-
-                    # Verificar se o servidor já marcou essa data, independentemente da carga horária
-                    if Ajuda_Custo.objects.filter(matricula=servidor.matricula, data=data_completa).exists():
-                        messages.error(self.request, f'O servidor já possui uma entrada para {dia}/{mes}/{ano}.')
-                        return self.form_invalid(form)
-
-                    # Verificar se já existe um registro para este mês (por matrícula)
-                    if not ajuda_custo_existente:
-                        ajuda_custo_existente = Ajuda_Custo.objects.filter(
-                            matricula=servidor.matricula,
-                            data__year=ano_int,
-                            data__month=mes_int
-                        ).first()
-
-                    # Cria o objeto Ajuda_Custo e salva no banco de dados, sem o arquivo de folha ainda
-                    ajuda_custo = Ajuda_Custo(
-                        matricula=self.request.user.matricula,
-                        nome=self.request.user.nome_completo,
-                        data=data_completa,
-                        unidade=unidade,
-                        carga_horaria=carga_horaria,
-                        majorado=DataMajorada.objects.filter(data=data_completa).exists(),
-                    )
-                    ajuda_custo.save()
-
-                except ValueError:
-                    messages.error(self.request, f'Erro: Data inválida - {dia}/{mes}/{ano}')
-                    return self.form_invalid(form)
-
-            # Fora do loop, salvamos o arquivo apenas uma vez para o mês
-            if folha_assinada:
-
-                if folha_assinada:
-                    print(f"Arquivo recebido: {folha_assinada.name}")  # Verifica o nome do arquivo recebido
-                else:
-                    print("Nenhum arquivo foi enviado.")
-                if ajuda_custo_existente:
-                    # Se já havia um registro anterior, substituímos o arquivo
-                    ajuda_custo_existente.folha_assinada = folha_assinada
-                    ajuda_custo_existente.save()
-                else:
-                    # Atribuímos o arquivo ao último registro salvo (ou poderia ser o primeiro)
-                    ajuda_custo.folha_assinada = folha_assinada
-                    ajuda_custo.save()
-
-            messages.success(self.request, 'Datas adicionadas com sucesso!')
-            return redirect(self.success_url)
         else:
             messages.error(self.request, 'Erro no Cadastro, Confira os Dados e Tente Novamente.')
             return self.form_invalid(form)
-
 
 
 class AdminCadastrar(LoginRequiredMixin, UserPassesTestMixin, FormView):
