f2d438cede4533775dd01ac11154d4855deb3c84
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-10-08 20:47:59 -0300

    admin cadastrar mobile

--- ajuda_custo/admin.py
+++ ajuda_custo/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from .models import DataMajorada, Ajuda_Custo, LimiteAjudaCusto
+from .models import Ajuda_Custo, DataMajorada, LimiteAjudaCusto
 
-# Register your models here.
 
+@admin.register(Ajuda_Custo)
+class AjudaCustoAdmin(admin.ModelAdmin):
+    # Definir quais campos aparecerão na listagem de Ajuda_Custo
+    list_display = ('nome', 'matricula', 'data', 'unidade', 'carga_horaria', 'majorado')
+
+    # Definir os campos que podem ser pesquisados (campo de pesquisa no topo da lista)
+    search_fields = ('nome', 'matricula', 'data')
+
+    # Adicionar filtros laterais por unidade, carga horária e se é majorado ou não
+    list_filter = ('unidade', 'carga_horaria', 'majorado', 'data')
+
+    # Habilitar a ordenação por data
+    ordering = ('-data',)
+
+
+# Registrando outros modelos
 admin.site.register(DataMajorada)
-
-admin.site.register(Ajuda_Custo)
-
 admin.site.register(LimiteAjudaCusto)
-
-
