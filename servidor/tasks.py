1d0a2480d103822a496a5a88c22f55e1434dd9fb
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-10-16 15:49:30 -0300

    configuracao celery e redis

--- servidor/tasks.py
+++ servidor/tasks.py
@@ -1,18 +1,24 @@
 from celery import shared_task
-from celery import shared_task
 from django.template.loader import get_template
-from django.http import HttpResponse
+from django.core.files.base import ContentFile
+from django.conf import settings
 from xhtml2pdf import pisa
 import os
-from django.conf import settings
+
 
 @shared_task
 def gerar_pdf(context, template_path):
     template = get_template(template_path)
     html = template.render(context)
-    output_path = os.path.join(settings.MEDIA_ROOT, 'relatorio_servidores.pdf')
-    with open(output_path, 'wb') as output:
-        pisa_status = pisa.CreatePDF(html, dest=output)
+    output = ContentFile('')
+
+    pisa_status = pisa.CreatePDF(html, dest=output)
+
     if pisa_status.err:
         return 'Erro ao gerar PDF'
+
+    output_path = os.path.join(settings.MEDIA_ROOT, 'relatorio_servidores.pdf')
+    with open(output_path, 'wb') as f:
+        f.write(output.read())
+
     return output_path
