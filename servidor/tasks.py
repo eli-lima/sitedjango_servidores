e557cb082c57dfed748206893dcdfb5631c79f1c
eli-lima <139256370+eli-lima@users.noreply.github.com>
2024-10-18 15:23:08 -0300

    voltando ao original .2

--- servidor/tasks.py
+++ servidor/tasks.py
@@ -1,31 +1,38 @@
 from celery import shared_task
 from django.template.loader import render_to_string
-from django.conf import settings
 import os
 from xhtml2pdf import pisa
 from cloudinary.uploader import upload as cloudinary_upload
-import psutil
+import gc  # Para liberar a memória manualmente após o processamento
+import tempfile  # Para a criação de arquivos temporários
+
 
 @shared_task
 def generate_pdf(servidores, template_path):
     try:
         print("Starting PDF generation...")
         html = render_to_string(template_path, {'servidores': servidores})
-        output_path = os.path.join('/tmp', 'relatorio_servidores.pdf')
-        with open(output_path, 'wb') as output:
+
+        # Usando NamedTemporaryFile para evitar criação de arquivos permanentes e gerenciar melhor o espaço em memória
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".pdf") as output:
             pisa_status = pisa.CreatePDF(html.encode('utf-8'), dest=output)
-        if pisa_status.err:
-            print("Error creating PDF")
-            return 'Erro ao gerar PDF'
-        print("PDF created successfully")
-        print(f"Memory usage after creating PDF: {psutil.virtual_memory().percent}%")
-        # Upload do PDF para o Cloudinary
-        response = cloudinary_upload(output_path, resource_type='raw')
-        cloudinary_url = response['url']
-        # Deletar o arquivo temporário
-        os.remove(output_path)
+            if pisa_status.err:
+                print("Error creating PDF")
+                return 'Erro ao gerar PDF'
+
+            output.flush()  # Garante que todo conteúdo seja escrito
+            output.seek(0)  # Vai para o início do arquivo para fazer o upload
+
+            # Upload do PDF para o Cloudinary
+            response = cloudinary_upload(output.name, resource_type='raw')
+            cloudinary_url = response['url']
+
+        # Força o garbage collector para liberar memória
+        gc.collect()
+
         print("PDF uploaded to Cloudinary successfully")
         return cloudinary_url
+
     except Exception as e:
         print(f"Error generating PDF: {e}")
         return 'Erro ao gerar PDF'
