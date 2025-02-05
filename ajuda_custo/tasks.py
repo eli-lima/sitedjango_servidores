bef4574173edaab6c25a9c6b93fcb796bfa6e235
eli-lima <139256370+eli-lima@users.noreply.github.com>
2025-02-05 20:58:58 -0300

    modulo copen

--- ajuda_custo/tasks.py
+++ ajuda_custo/tasks.py
@@ -8,43 +8,14 @@ import pandas as pd
 from celery import shared_task
 import requests
 from collections import defaultdict
-from django.contrib import messages
 
 
 @shared_task
 def process_batch(df_batch):
     registros_inseridos = False
     ajuda_custos_para_inserir = []
-    horas_por_servidor = defaultdict(int)  # Dicionário para armazenar as horas acumuladas por servidor e mês
     datas_processadas = set()
     erros = []  # Lista para armazenar as informações sobre falhas
-
-    # Pré-processamento: Agrupa as matrículas e datas para consulta única no banco
-    matriculas_datas = [(re.sub(r'\D', '', str(row['Matrícula'])).lstrip('0'), parser.parse(str(row['Data'])).date())
-                        for row in df_batch if row['Matrícula']]
-
-    print(f'matriculas banco de dados: {matriculas_datas}')
-
-    # Consulta única para verificar registros existentes no banco
-    registros_existentes = Ajuda_Custo.objects.filter(
-        matricula__in=[md[0] for md in matriculas_datas],
-        data__in=[md[1] for md in matriculas_datas]
-    ).values('matricula', 'data', 'carga_horaria')
-
-    # Cria um conjunto de tuplas (matrícula, data) para verificação rápida de duplicidade
-    registros_existentes_set = {(r['matricula'], r['data']) for r in registros_existentes}
-
-    # Pré-calcula as horas acumuladas por servidor e mês
-    for registro in registros_existentes:
-        matricula = registro['matricula']
-        data = registro['data']
-        mes_ano = (data.year, data.month)
-        carga_horaria_passado = registro['carga_horaria'].strip()
-
-        if carga_horaria_passado == "12 horas":
-            horas_por_servidor[(matricula, mes_ano)] += 12
-        elif carga_horaria_passado == "24 horas":
-            horas_por_servidor[(matricula, mes_ano)] += 24
 
     for row in df_batch:  # Agora df_batch é uma lista de dicionários
         matricula_raw = row['Matrícula']
@@ -85,28 +56,13 @@ def process_batch(df_batch):
             erros.append(f"Registro duplicado para o servidor {nome} na data {data_completa}.")
             continue
 
-        # Verificar no banco de dados se já existe um registro para a mesma matrícula e data
-        if (servidor.matricula, data_completa) in registros_existentes_set:
+        # **Verificar no banco de dados se já existe um registro para a mesma matrícula e data**
+        registro_existente = Ajuda_Custo.objects.filter(matricula=servidor.matricula, data=data_completa).exists()
+        if registro_existente:
             erros.append(f"Erro: Registro já existe para o servidor {nome} na data {data_completa}.")
             continue
 
         datas_processadas.add((servidor.matricula, data_completa))
-
-        # Extraindo mês e ano da data
-        mes_ano = (data_completa.year, data_completa.month)
-
-        # Verificar se a soma da carga horária do mês excede 192 horas
-        horas_mes_atual = horas_por_servidor[(servidor.matricula, mes_ano)] + carga_horaria
-        print(f'horas do mes atual: {horas_mes_atual}')
-
-        if horas_mes_atual > 192:
-            erros.append(
-                f"Limite mensal de 192 horas excedido para o servidor {nome} no mês {data_completa.strftime('%m/%Y')}.")
-            continue
-
-        # Atualiza o dicionário com as horas acumuladas
-        horas_por_servidor[(servidor.matricula, mes_ano)] = horas_mes_atual
-        print(f'dicionarios horas por servidor: {horas_por_servidor}')
 
         majorado = DataMajorada.objects.filter(data=data_completa).exists()
 
@@ -143,21 +99,23 @@ def process_batch(df_batch):
 
 @shared_task(bind=True)
 def process_excel_file(self, cloudinary_url):
+
     try:
         # Fazer o download do arquivo do Cloudinary
         response = requests.get(cloudinary_url)
         response.raise_for_status()
 
+
         # Ler o arquivo Excel
         df = pd.read_excel(response.content)
 
+
         # Lógica de processamento dos dados
-        batch_size = 10000  # Tamanho do lote (ajuste conforme necessário)
+        batch_size = 10000
         total_registros = df.shape[0]
         print(f"Total de registros a processar: {total_registros}")
         erros_totais = []
 
-        # Processa o arquivo em lotes
         for start in range(0, total_registros, batch_size):
             end = min(start + batch_size, total_registros)
             df_batch = df.iloc[start:end]
@@ -166,7 +124,7 @@ def process_excel_file(self, cloudinary_url):
             df_batch_dict = df_batch.to_dict(orient='records')
             print(f"Processando lote de registros: {start} a {end}")
 
-            # Chama a função process_batch para processar o lote
+            # Processa o lote e acumula erros
             result = process_batch(df_batch_dict)  # Chama a função que já processa os dados
             erros_totais.extend(result)
 
