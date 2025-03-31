33e6c2ebc790468b8815ae1f65077cbda66345c8
eli-lima <elyaislan@gmail.com>
2025-03-31 00:17:33 -0300

    copen atualizacoes

--- seappb/templatetags/custom_filters.py
+++ seappb/templatetags/custom_filters.py
@@ -6,3 +6,12 @@ register = template.Library()
 @register.filter
 def add_class(field, css_class):
     return field.as_widget(attrs={'class': css_class})
+
+
+@register.filter
+def get_attribute(obj, attr):
+    """Filtro para acessar atributos de um objeto dinamicamente."""
+    try:
+        return getattr(obj, attr)
+    except AttributeError:
+        return ''
