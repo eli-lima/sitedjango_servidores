4e4a7293f53f77d3866cb0656a615c0a41216aac
eli-lima <elyaislan@gmail.com>
2025-04-03 20:54:25 -0300

    data django admin

--- ajuda_custo/admin.py
+++ ajuda_custo/admin.py
@@ -1,47 +1,22 @@
 from django.contrib import admin
 from rangefilter.filters import DateRangeFilter
-from django.http import HttpRequest
-from datetime import datetime
 from .models import Ajuda_Custo, DataMajorada, LimiteAjudaCusto, CotaAjudaCusto
+
+
+
 
 
 @admin.register(Ajuda_Custo)
 class AjudaCustoAdmin(admin.ModelAdmin):
-    # Configuração original dos campos de exibição
     list_display = ('nome', 'matricula', 'data', 'unidade', 'carga_horaria', 'majorado', 'codigo_verificacao')
-
-    # Campos de pesquisa
     search_fields = ('nome', 'matricula', 'data')
-
-    # Filtros (incluindo o DateRangeFilter corrigido)
     list_filter = (
         'unidade',
         'carga_horaria',
         'majorado',
-        ('data', DateRangeFilter),  # Filtro de intervalo de datas
+        ('data', DateRangeFilter),  # Filtro de intervalo com calendário
     )
-
-    # Ordenação padrão
     ordering = ('-data',)
-
-    # Correção do formato de data (método adicionado)
-    def changelist_view(self, request, extra_context=None):
-        if 'data__range__gte' in request.GET and 'data__range__lte' in request.GET:
-            try:
-                # Converte datas de DD/MM/YYYY para YYYY-MM-DD
-                data_inicio = request.GET['data__range__gte']
-                data_fim = request.GET['data__range__lte']
-
-                data_inicio_formatada = datetime.strptime(data_inicio, '%d/%m/%Y').strftime('%Y-%m-%d')
-                data_fim_formatada = datetime.strptime(data_fim, '%d/%m/%Y').strftime('%Y-%m-%d')
-
-                request.GET = request.GET.copy()
-                request.GET['data__range__gte'] = data_inicio_formatada
-                request.GET['data__range__lte'] = data_fim_formatada
-            except ValueError:
-                pass  # Mantém o valor original se a conversão falhar
-
-        return super().changelist_view(request, extra_context)
 
 
 # Registrando outros modelos
